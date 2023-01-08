6bb716fda7fbe07368d893574230d95f6a24e070
Piotr Kilczuk <piotr@kilczuk.dev>
2023-01-08 19:27:17 +0100

    Ability to work out what needs to be created/updated/closed

--- wrike_todoist/__main__.py
+++ wrike_todoist/__main__.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
+import json
 from os import environ
+from pprint import pformat
 
-from requests import get
+from requests import get, post
 
 from wrike_todoist import models
 
 WRIKE_ACCESS_TOKEN = environ["WRIKE_ACCESS_TOKEN"]
 TODOIST_ACCESS_TOKEN = environ["TODOIST_ACCESS_TOKEN"]
 TODOIST_PROJECT_NAME = environ["TODOIST_PROJECT_NAME"]
+TODOIST_LABEL = environ["TODOIST_LABEL"]
 
 wrike_user_response = get(
     "https://www.wrike.com/api/v4/contacts?me=true",
@@ -21,6 +24,9 @@ wrike_tasks_response = get(
         "access_token": WRIKE_ACCESS_TOKEN,
         "status": "Active",
         "responsibles": f"[{wrike_user.id}]",
+        "subTasks": True,
+        "fields": "[description,briefDescription,superTaskIds,subTaskIds]",
+        "limit": 500,
     },
 )
 wrike_tasks = models.WrikeTaskCollection.from_response(wrike_tasks_response.json())
@@ -34,15 +40,49 @@ todoist_projects = models.TodoistProjectCollection.from_response(todoist_project
 
 todoist_project = todoist_projects[TODOIST_PROJECT_NAME]
 
-raise NotImplementedError(todoist_project)
+todoist_labels_response = get(
+    "https://api.todoist.com/rest/v2/labels",
+    headers={"Authorization": f"Bearer {TODOIST_ACCESS_TOKEN}"},
+)
+
+todoist_labels = models.TodoistLabelCollection.from_response(todoist_labels_response.json())
+
+try:
+    todoist_label = todoist_labels[TODOIST_LABEL]
+except KeyError:
+    todoist_label = models.TodoistLabel(id=models.PendingValue(), name=TODOIST_LABEL)
+
+    todoist_label_response = post(
+        "https://api.todoist.com/rest/v2/labels",
+        headers={"Authorization": f"Bearer {TODOIST_ACCESS_TOKEN}"},
+        json=todoist_label.serialize(),
+    )
+
+    todoist_label = models.TodoistLabel.from_response(todoist_label_response.json())
 
 todoist_tasks_response = get(
     "https://api.todoist.com/rest/v2/tasks",
-    params={"project_id": TODOIST_PROJECT_ID},
+    params={"project_id": todoist_project.id, "label": TODOIST_LABEL},
     headers={"Authorization": f"Bearer {TODOIST_ACCESS_TOKEN}"},
 )
 actual_todoist_tasks = models.TodoistTaskCollection.from_response(todoist_tasks_response.json())
 
-expected_todoist_tasks = models.TodoistTaskCollection.from_wrike_tasks(wrike_tasks, TODOIST_PROJECT_ID)
+expected_todoist_tasks = models.TodoistTaskCollection.from_wrike_tasks(wrike_tasks, todoist_project.id)
 
-raise NotImplementedError(len(actual_todoist_tasks), len(expected_todoist_tasks))
+comparison_result = models.TodoistTaskCollection.compare(expected_todoist_tasks, actual_todoist_tasks)
+
+for todoist_task in comparison_result.to_add:
+    create_task_response = post(
+        "https://api.todoist.com/rest/v2/tasks",
+        headers={"Authorization": f"Bearer {TODOIST_ACCESS_TOKEN}"},
+        json=todoist_task.serialize(),
+    )
+    print(json.dumps(todoist_task.serialize()))
+    # todoist_task.update_from_response(create_task_response.json())
+    raise NotImplementedError(create_task_response.text, todoist_task)
+
+for todoist_task in comparison_result.to_update:
+    raise NotImplementedError(todoist_task)
+
+
+raise NotImplementedError(len(actual_todoist_tasks), len(expected_todoist_tasks), pformat(comparison_result))
--- wrike_todoist/models.py
+++ wrike_todoist/models.py
@@ -1,12 +1,26 @@
 from __future__ import annotations
 
 import dataclasses
-from typing import Dict, List, Type, Optional, Union
+from typing import Dict, List, Type, Optional, Union, NamedTuple
 
 
-class PendingPrimaryKey:
+class PendingValue:
     def __hash__(self):
         return id(self)
+
+    def __repr__(self):
+        return f"<PendingValue #{id(self)}>"
+
+
+class Item:
+    def serialize(self) -> Dict:
+        data = {}
+        for field in dataclasses.fields(self):
+            name = field.name
+            value = getattr(self, name)
+            if not isinstance(value, PendingValue):
+                data[name] = value
+        return data
 
 
 class Collection:
@@ -16,6 +30,9 @@ class Collection:
     def __len__(self):
         return len(self.members)
 
+    def __contains__(self, item):
+        return item.primary_key in self.members
+
     def __getitem__(self, item):
         return self.members[item]
 
@@ -24,7 +41,7 @@ class Collection:
 
 
 @dataclasses.dataclass
-class WrikeUser:
+class WrikeUser(Item):
     id: str
 
     @property
@@ -37,7 +54,7 @@ class WrikeUser:
 
 
 @dataclasses.dataclass
-class WrikeTask:
+class WrikeTask(Item):
     id: str
     title: str
     permalink: str
@@ -46,6 +63,10 @@ class WrikeTask:
     def primary_key(self) -> str:
         return self.id
 
+    @property
+    def numeric_id(self) -> int:
+        return int(self.permalink.split("=")[-1])
+
     @classmethod
     def from_response(cls, response: Dict) -> WrikeTask:
         return cls(id=response["id"], title=response["title"], permalink=response["permalink"])
@@ -53,7 +74,7 @@ class WrikeTask:
 
 @dataclasses.dataclass
 class WrikeTaskCollection(Collection):
-    members: Dict[Union[str, PendingPrimaryKey], WrikeTask]
+    members: Dict[Union[str, PendingValue], WrikeTask]
     type = WrikeTask
 
     @classmethod
@@ -62,7 +83,7 @@ class WrikeTaskCollection(Collection):
 
 
 @dataclasses.dataclass
-class TodoistProject:
+class TodoistProject(Item):
     id: int
     name: str
 
@@ -77,7 +98,7 @@ class TodoistProject:
 
 @dataclasses.dataclass
 class TodoistProjectCollection(Collection):
-    members: Dict[Union[int, PendingPrimaryKey], TodoistTask]
+    members: Dict[Union[int, PendingValue], TodoistTask]
     type = TodoistProject
 
     @classmethod
@@ -86,48 +107,111 @@ class TodoistProjectCollection(Collection):
 
 
 @dataclasses.dataclass
-class TodoistTask:
+class TodoistTask(Item):
+    id: Union[int, PendingValue]
     content: str
     description: str
     project_id: str
     parent_id: Optional[str]
-    priority: int
+    # priority: int
     labels: List[str]
+
+    parent: Optional[TodoistTask] = None
+    children: List[TodoistTask] = dataclasses.field(default_factory=list)
+
+    @property
+    def primary_key(self) -> str:
+        return self.content
 
     @classmethod
     def from_response(cls, response: Dict) -> TodoistTask:
         return cls(
+            id=response["id"],
             content=response["content"],
             description=response["description"],
             project_id=response["project_id"],
             parent_id=response["parent_id"],
-            priority=response["priority"],
+            # priority=response["priority"],
             labels=response["labels"],
         )
+
+    def update_from_response(self, response: Dict):
+        self.id = response.get("id") or self.id
+        self.content = response.get("content") or self.content
+        self.description = response.get("description") or self.description
+
+
+class TaskComparisonResult(NamedTuple):
+    to_add: TodoistTaskCollection
+    to_update: TodoistTaskCollection
+    to_complete: TodoistTaskCollection
 
 
 @dataclasses.dataclass
 class TodoistTaskCollection(Collection):
-    members: Dict[Union[str, PendingPrimaryKey], TodoistTask]
+    members: Dict[Union[str, PendingValue], TodoistTask] = dataclasses.field(default_factory=dict)
     type = TodoistTask
 
     @classmethod
-    def from_response(cls, response: List[Dict]) -> Collection:
-        return cls(members={item["id"]: cls.type.from_response(item) for item in response})
+    def from_response(cls, response: List[Dict]) -> TodoistTaskCollection:
+        return cls(members={item["content"]: cls.type.from_response(item) for item in response})
 
     @classmethod
     def from_wrike_tasks(cls, wrike_tasks: WrikeTaskCollection, todoist_project_id: str) -> TodoistTaskCollection:
         tasks = {}
         for wrike_task in wrike_tasks:
-            primary_key = PendingPrimaryKey()
-            description = f"[{wrike_task.id}] {wrike_task.title}"
+            primary_key = PendingValue()
+            content = f"[#{wrike_task.numeric_id}] {wrike_task.title}"
             todoist_task = TodoistTask(
-                content=wrike_task.permalink,
-                description=description,
+                id=PendingValue(),
+                description=wrike_task.permalink,
+                content=content,
                 project_id=todoist_project_id,
                 parent_id=None,
-                priority=0,
+                # priority=0,
                 labels=["Wrike"],
             )
             tasks[primary_key] = todoist_task
         return cls(members=tasks)
+
+    @classmethod
+    def compare(cls, wrike_tasks: TodoistTaskCollection, todoist_tasks: TodoistTaskCollection) -> TaskComparisonResult:
+        to_add = TodoistTaskCollection()
+        to_update = TodoistTaskCollection()
+        to_complete = TodoistTaskCollection()
+
+        print(todoist_tasks)
+
+        for wrike_task in wrike_tasks:
+
+            print(wrike_task)
+
+            if wrike_task in todoist_tasks:
+                to_update.members[wrike_task.primary_key] = wrike_task
+            else:
+                to_add.members[wrike_task.primary_key] = wrike_task
+        return TaskComparisonResult(to_add=to_add, to_update=to_update, to_complete=to_complete)
+
+
+@dataclasses.dataclass
+class TodoistLabel(Item):
+    id: Union[int, PendingValue]
+    name: str
+
+    @property
+    def primary_key(self) -> str:
+        return self.name
+
+    @classmethod
+    def from_response(cls, response: Dict) -> TodoistLabel:
+        return cls(id=response["id"], name=response["name"])
+
+
+@dataclasses.dataclass
+class TodoistLabelCollection(Collection):
+    members: Dict[Union[str, PendingValue], TodoistLabel]
+    type = TodoistLabel
+
+    @classmethod
+    def from_response(cls, response: List[Dict]) -> Collection:
+        return cls(members={item["name"]: cls.type.from_response(item) for item in response})
